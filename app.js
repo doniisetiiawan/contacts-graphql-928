409a3f481ffd94a798743c9fe57cefffd1d1415b
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-07 14:18:26 +0700

    Add methods

--- app.js
+++ app.js
@@ -2,16 +2,46 @@ import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import { buildSchema } from 'graphql';
 
+import { contacts } from './data/contacts';
+
 const app = express();
 
 const schema = buildSchema(`
 type Query {
-  message: String
+  contact(id: Int!): Contact
+  contacts(name: String): [Contact]
+}
+
+type Contact {
+  id: Int
+  name: String
+  phone: String
+  email: String
 }
 `);
 
+const methods = {
+  getContact: (args) => {
+    const { id } = args;
+
+    return contacts.filter(contact => contact.id === id)[0];
+  },
+  getContacts: (args) => {
+    const { name = false } = args;
+
+    if (!name) {
+      return contacts;
+    }
+
+    return contacts.filter(
+      contact => contact.name.includes(name),
+    );
+  },
+};
+
 const root = {
-  message: () => 'First message',
+  contact: methods.getContact,
+  contacts: methods.getContacts,
 };
 
 app.use('/graphql', graphqlHTTP({
